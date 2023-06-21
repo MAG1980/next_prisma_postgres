98efc8ebeda76673b0f3e21e613dc9346164c20d
MAG1980 <al-1980@inbox.ru>
2023-06-21 14:48:36 +0300

    Создан seeder для модели Client. Для генерации уникальных данных использован пакет enforce-unique.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,37 @@
 import {PrismaClient} from "@prisma/client";
-import { faker } from '@faker-js/faker';
+import {faker} from '@faker-js/faker';
+import {EnforceUniqueError, UniqueEnforcer} from 'enforce-unique';
 
 const prisma = new PrismaClient()
+
+const uniqueEnforcerLogin = new UniqueEnforcer();
+const uniqueEnforcerEmail = new UniqueEnforcer();
+const uniqueEnforcerPhone = new UniqueEnforcer();
+
+const getLogin = (): string => uniqueEnforcerLogin.enforce(() => {
+    return faker.internet.userName()
+})
+
+const getEmail = (firstName: string, lastName: string): string => uniqueEnforcerEmail.enforce(() => {
+    return faker.internet.email({firstName, lastName});
+});
+
+const getPhoneNumber = (): string => uniqueEnforcerPhone.enforce(() => {
+    return faker.phone.number()
+})
+
+const getClient = () => {
+    const firstName = faker.person.firstName();
+    const lastName = faker.person.lastName();
+    return {
+        firstName,
+        lastName,
+        login: getLogin(),
+        email: getEmail(firstName, lastName),
+        phoneNumber: getPhoneNumber()
+    }
+}
+
 
 async function main() {
     //Если использовать create вместо upsert, то при каждом посеве client создавался бы заново
@@ -11,26 +41,21 @@ async function main() {
         update: {},
         //выполнится только в том случае, если client не существует
         create: {
-            login:'Login',
+            login: 'Login',
             email: 'test@test.com',
             firstName: 'Test User',
-            lastName:'Test User Lastname',
+            lastName: 'Test User Lastname',
             phoneNumber: "+75554443322",
         }
     })
     console.log(client)
-    
+
     //Используем Faker
     for (let i = 0; i < 10; i++) {
+
         await prisma.client.create(
             {
-                data: {
-                    login:faker.helpers.unique(faker.animal.type),
-                    email: faker.helpers.unique(faker.internet.email),
-                    firstName: faker.person.firstName(),
-                    lastName: faker.person.lastName(),
-                    phoneNumber: faker.helpers.unique(faker.phone.number)
-                }
+                data: {...getClient()}
             }
         )
     }
