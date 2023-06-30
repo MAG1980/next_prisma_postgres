fc92ebc20046b95324a86eb1670b5366bdf8bfa3
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2023-06-30 19:39:04 +0300

    Отношения вида многие ко многим между Orders и Goods описаны в неявной форме. Автоматически было создано ограничение уникальности между Orders и Goods, т.е. в заказ могут входить только уникальные товары. "Посев" выдаёт ошибки.

--- components/Test.tsx
+++ components/Test.tsx
@@ -1,6 +1,6 @@
 import { Prisma } from '@prisma/client'
 import { prisma } from "@/lib/prisma";
-import { Client, Good, GoodsOnOrders, Order } from "@prisma/client";
+import { Client, Good, Order } from "@prisma/client";
 
 const clientsOrdersGoods = Prisma.validator<Prisma.ClientInclude>()({
     orders: true,
